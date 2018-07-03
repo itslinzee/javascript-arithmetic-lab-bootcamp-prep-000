edf2a417b6500801be5c1e5699ede7cd897274ac
Lindsay Ortiz <lindsay.ortiz@live.com>
2018-07-03 21:18:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
+}
+
+function inc(n) {
+  n += 1;
+  return n;
+}
+
+function dec(n) {
+  n -= 1;
+  return n;
 }
 
